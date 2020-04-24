90c898126e34e582ccc8ef138d5b02d9719e992c
André Sier <astronaut@s373.net>
2020-04-24 10:51:45 +0100

    extern C ? hmm

---
+++ example-boids-singer/src/boids-singer.cpp
@@ -0,0 +1,18 @@
+#include "ofMain.h"
+#include "ofxBoidsSinger2d.h"
+
+
+int main(){
+
+	ofxBoidsSinger2D
+
+					singerflock2d;
+
+	singerflock2d . setup(1, 0);
+
+	singerflock2d . update();
+
+	// draw
+
+	return 0;
+}
---
+++ src/ofxBoidsSinger2d.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+#include "boids2d.2.0a.c"
+#ifdef __cplusplus
+}
+#endif
+
+
+class ofxBoidsSinger2D {
+protected:
+
+	FlockPtr flock2d;
+
+public:
+
+	void setup(int nboids, int mode){
+
+	}
+
+	void update( ) {
+
+	}
+
+};
